83400da561e82fe70f9450cb0f2e1da39998902b
tuanpep <tuan.thanh@savvycomsoftware.com>
2023-03-28 10:14:25 +0700

    setup docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+});
